dd21d6135968fdd5041c69d4714ca24f5f84eb52
5h100ky <jaejin.baek@gmail.com>
2026-03-24 22:03:25 +0100

    Add files via upload

---
+++ atmega32u4/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xA3, 0x32, 0x4F, 0x6C, 0x8E, 0x1B, 0x5D, 0x79}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 1}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1}
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 3
